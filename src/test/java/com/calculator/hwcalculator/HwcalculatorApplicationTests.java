59d32c5b555544b14cbd06bc132bba8e98493a40
Normall <esenchukden@gmail.com>
2025-02-16 13:36:03 +0300

    initial commit

---
+++ src/test/java/com/calculator/hwcalculator/HwcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.hwcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
